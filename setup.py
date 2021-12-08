e4b0cff7b12d6c2c45c182cdd1f01775de68a38e
matt <philmattdev@gmail.com>
2021-12-09 02:33:16 +0800

    version update to 1.3

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name="fluxhelper",
     packages=["fluxhelper", "fluxhelper.jarvis"],
-    version="1.2",
+    version="1.3",
     license="MIT",
     description="Helper library made for my projects",
     author="Philippe Mathew",
     author_email="philmattdev@gmail.com",
     url="https://github.com/bossauh/fluxhelper",
-    download_url="https://github.com/bossauh/fluxhelper/archive/refs/tags/v_12.tar.gz",
+    download_url="https://github.com/bossauh/fluxhelper/archive/refs/tags/v_13.tar.gz",
     keywords=["helper"],
     install_requires=[
         "pycaw",
